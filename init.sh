fdb3913a91352dd444b0db4151bd6fb03d277886
burzik2020 <burzik2002@i.ua>
2021-11-22 10:47:41 +0200

    Update

---
+++ init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+helmfile_ver=0.142.0
+sudo apt-get update && \
+sudo apt-get -y install apt-transport-https ca-certificates \
+curl software-properties-common gnupg lsb-release python3-pip unzip git && \
+
+#docker+compose install
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+  
+sudo apt-get update && \
+sudo apt-cache policy docker-ce && \
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io && \
+sudo usermod -aG docker ${USER} && \
+sudo pip3 install docker-compose && \
+
+#aws cli install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+unzip awscliv2.zip && \
+sudo ./aws/install -u && \
+rm -rf awscliv2.zip && \
+
+#terraform cli install
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
+sudo apt-get update && sudo apt-get -y install terraform && \
+
+#kubectl install
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update && sudo apt-get install -y kubectl && \
+
+#helm install
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add - && \
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list && \
+sudo apt-get update && sudo apt-get -y install helm && \
+
+#helmfile install
+wget -O helmfile_linux_amd64 https://github.com/roboll/helmfile/releases/download/v${helmfile_ver}/helmfile_linux_amd64 && \
+chmod +x helmfile_linux_amd64 && \
+sudo mv helmfile_linux_amd64 /usr/bin/helmfile && \
+
+#Print OS and tool versions
+echo "OS details are: $(uname -a)" && echo "" && \
+echo "AWS Version is $(aws --version)" && echo "" && \
+echo "Kubectl version is $(kubectl version --client)" && echo "" && \
+echo "Terraform version is $(terraform version)" && echo "" && \
+git --version
